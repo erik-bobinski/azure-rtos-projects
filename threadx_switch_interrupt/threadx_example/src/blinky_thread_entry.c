bad79043f3e70e2abe3575db11e06fe494d21b96
Erik <123090127+erik-bobinski@users.noreply.github.com>
2025-02-14 01:48:46 -0500

    Update blinky_thread_entry.c

--- threadx_switch_interrupt/threadx_example/src/blinky_thread_entry.c
+++ threadx_switch_interrupt/threadx_example/src/blinky_thread_entry.c
@@ -4,10 +4,12 @@
 * SPDX-License-Identifier: BSD-3-Clause
 */
 
+/* main thread that sets LED pins to High/Low, toggles it for next thread run,
+gets semaphore for SW1, then waits perpetually for ISR callback fn to put SW1 semaphore, 
+thus proccing main thread to run with the LED pins toggled - 
+Results in a switch that toggles all LEDs present on board
+*/
 #include "blinky_thread.h"
-
-// sleep_ticks global var
-#include "extern.h"
 
 extern bsp_leds_t g_bsp_leds;
 
@@ -22,7 +24,7 @@ void blinky_thread_entry (void)
     {
         while (1)
         {
-            ;                          // There are no LEDs on this board
+            ; // There are no LEDs on this board
         }
     }
 
@@ -69,62 +71,3 @@ void blinky_thread_entry (void)
 void g_external_irq0_callback(external_irq_callback_args_t * p_args) {
     tx_semaphore_put(&g_sw1_semaphore0);
 }
-
-
-
-//// CODE FOR LED TOGGLE WITH S1
-//#include "blinky_thread.h"
-//
-//void blinky_thread_entry(void);
-//
-//extern bsp_leds_t leds = g_bsp_leds;
-//
-//void blinky_thread_entry(void) {
-//    bsp_io_level_t pin_level = BSP_IO_LEVEL_HIGH;
-//
-//    /* If this board has no LEDs then trap here */
-//        if (0 == leds.led_count)
-//        {
-//            while (1)
-//            {
-//                ;// There are no LEDs on this board
-//            }
-//        }
-//
-//        /* Enable access to the PFS registers.
-//         If using r_ioport module then register protection is automatically
-//         handled. This code uses BSP IO functions to show how it is used.
-//        */
-//    R_BSP_PinAccessEnable();
-//
-//    while(1) {
-//
-//        /* Update all board LEDs */
-//        for (uint32_t i = 0; i < leds.led_count; i++)
-//        {
-//            /* Get pin to toggle */
-//            uint32_t pin = leds.p_leds[i];
-//
-//            /* Write to this pin */
-//            R_BSP_PinWrite((bsp_io_port_pin_t) pin, pin_level);
-//        }
-//
-//
-//        if (BSP_IO_LEVEL_LOW == pin_level)
-//            {
-//                pin_level = BSP_IO_LEVEL_HIGH;
-//            }
-//            else
-//            {
-//                pin_level = BSP_IO_LEVEL_LOW;
-//            }
-//
-//        tx_semaphore_get(&g_s1_semaphore0, TX_WAIT_FOREVER);
-//
-//    }
-//}
-//
-//void external_irq0_callback(external_irq_callback_args_t * p_args) {
-//    tx_semaphore_put(&g_s1_semaphore0);
-//}
-
